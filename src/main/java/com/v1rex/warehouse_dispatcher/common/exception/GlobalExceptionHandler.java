7595aee94e61e62c08dfb9d87062f98584c7357c
Amine <medaminebahij02@gmail.com>
2026-05-10 16:18:30 +0200

    chore(loging): logged GlobalExceptionHandler.

--- src/main/java/com/v1rex/warehouse_dispatcher/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/v1rex/warehouse_dispatcher/common/exception/GlobalExceptionHandler.java
@@ -2,14 +2,18 @@ package com.v1rex.warehouse_dispatcher.common.exception;
 
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -17,52 +21,65 @@ public class GlobalExceptionHandler {
             ResourceNotFoundException ex,
             HttpServletRequest request) {
 
-        ApiError error = new ApiError(
-                ex.getMessage(),
-                HttpStatus.NOT_FOUND.value(),
-                LocalDateTime.now(),
-                request.getRequestURI()
-        );
 
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        return createErrorResponse(ex.getMessage(),
+                HttpStatus.NOT_FOUND,
+                request);
     }
 
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ApiError> handleIllegalState(
+            IllegalStateException ex,
+            HttpServletRequest request) {
 
 
+        log.warn("Business rule violation at {}: {}",
+                request.getRequestURI(),
+                ex.getMessage());
+
+        return createErrorResponse(ex.getMessage(), HttpStatus.CONFLICT, request);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> handleGeneralException(
             Exception ex,
             HttpServletRequest request) {
 
+
+        log.error("Unhandled exception occurred at {} : ", request.getRequestURI(), ex);
+
+        return createErrorResponse(
+                "An unexpected error occurred.",
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                request
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleValidationException(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request) {
+
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+
+        log.debug("Validation failed at {}: {}", request.getRequestURI(), errorMessage);
+
+        return createErrorResponse("Validation Failed: " + errorMessage, HttpStatus.BAD_REQUEST, request);
+    }
+
+
+    private ResponseEntity<ApiError> createErrorResponse(String message, HttpStatus status, HttpServletRequest request) {
         ApiError error = new ApiError(
-                "An unexpected error occurred. Please try again later.",
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                message,
+                status.value(),
                 LocalDateTime.now(),
                 request.getRequestURI()
         );
-
-        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(error, status);
     }
-
-@ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
-public ResponseEntity<ApiError> handleValidationException(
-        org.springframework.web.bind.MethodArgumentNotValidException ex,
-        HttpServletRequest request) {
-
-    String errorMessage = ex.getBindingResult().getFieldErrors().stream()
-            .map(error -> error.getField() + ": " + error.getDefaultMessage())
-            .collect(java.util.stream.Collectors.joining(", "));
-
-    ApiError error = new ApiError(
-            "Validation Failed: " + errorMessage,
-            HttpStatus.BAD_REQUEST.value(),
-            LocalDateTime.now(),
-            request.getRequestURI()
-    );
-
-    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-}
 }
 
 
