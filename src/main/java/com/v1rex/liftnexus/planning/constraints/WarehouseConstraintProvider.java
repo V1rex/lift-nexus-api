bd493379767123e8fba2827004e9d1816b98d24e
Amine Bahij <go4computergeek@gmail.com>
2026-05-28 12:11:47 +0200

    feat(constraints): integrated implemented constraints into WarehouseConstraintProvider

--- src/main/java/com/v1rex/liftnexus/planning/constraints/WarehouseConstraintProvider.java
+++ src/main/java/com/v1rex/liftnexus/planning/constraints/WarehouseConstraintProvider.java
@@ -8,65 +8,14 @@ public class WarehouseConstraintProvider implements ConstraintProvider {
 
   @Override
   public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
-    return new Constraint[] {
-      ForkliftCapacityConstraint.forkliftCapacity(constraintFactory),
-    };
+      return new Constraint[] {
+            ForkliftCapacityConstraint
+                    .forkliftCapacity(constraintFactory),
+            ForkliftTravelDistanceConstraint
+                    .forkliftTravelDistance(constraintFactory),
+            TransportOrderEquipmentRequirementConstraint
+                    .equipmentType(constraintFactory)
+      };
   }
-
-  Constraint forkliftCapacity(ConstraintFactory constraintFactory) {
-    return ForkliftCapacityConstraint.forkliftCapacity(constraintFactory);
-  }
-  /*
-
-
-    // Hard constraint: check if the assigned transportOrders to a Forklift is
-    // compatible with the requirement equipment type of the transportorder
-    private Constraint taskEquipmentRequirement(ConstraintFactory factory) {
-      return factory
-          .forEach(TransportOrder.class)
-          .filter(transportorder -> transportorder.getForklift() != null)
-          .filter(transportorder -> transportorder.getRequiredEquipment() != transportorder.getForklift().getEquipmentType())
-          .penalize(HardSoftScore.ONE_HARD)
-          .asConstraint("TransportOrder equipment type requirement");
-    }
-
-    // Soft constraint: sum complete travel distance of the forklift
-    private Constraint minimizeTravelDistance(ConstraintFactory factory) {
-      return factory
-          .forEach(Forklift.class)
-          .filter(forklift -> !forklift.getTransportOrders().isEmpty())
-          .penalize(
-              HardSoftScore.ONE_SOFT,
-              forklift -> {
-                int totalTraveledDistance = 0;
-
-                List<TransportOrder> transportOrders = forklift.getTransportOrders();
-                // initial drive to the first transportorder
-                totalTraveledDistance +=
-                    (int) forklift.getCurrentStorageBin().distanceTo(transportOrders.get(0).getPickStorageBin());
-
-                for (int i = 0; i < transportOrders.size(); i++) {
-                  TransportOrder current = transportOrders.get(i);
-
-                  // We calculate the travel distance from currentTask
-                  // to the Delivery StorageBin
-                  totalTraveledDistance +=
-                      (int) current.getPickStorageBin().distanceTo(current.getDeliveryStorageBin());
-
-                  // if there is a next transportorder, we calculate the travel distance
-                  // from the delivery storagebin to the pick storagebin
-                  // of the next transportorder
-                  if (i < transportOrders.size() - 1) {
-                    TransportOrder next = transportOrders.get(i + 1);
-                    totalTraveledDistance +=
-                        (int) current.getDeliveryStorageBin().distanceTo(next.getPickStorageBin());
-                  }
-                }
-                // todo: think about the metrics!!
-                return totalTraveledDistance;
-              })
-          .asConstraint("Minimize travel distance");
-    }
-  */
 
 }
